e00e4df09bec5ff2e86aaafd53ee44c61aad1711
HajaSC <hajasc@protonmail.com>
2020-07-12 01:52:14 -0400

    Initial Commit - Adds Python Flask Server and Index.HTML

--- testServer/app.py
+++ testServer/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify, request, render_template
+from flask_cors import CORS
 
 
 app=Flask(__name__)
+CORS(app)
 
 @app.route('/hello', methods=['GET', 'POST'])
+@cross_origin()
 def hello():
 
     if request.method == 'POST':
@@ -12,7 +15,7 @@ def hello():
         return 'OK', 200
 
     else:
-        message = {'greeting':'Hello from Flask!'}
+        message = {'greeting':'Hello from Flask'}
         return jsonify(message)
 
 @app.route('/test')
